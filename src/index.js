7a793a55328868570654fac01ed5bbc43b5deaf0
Tasnim Alam <tasnimalamcse@gmail.com>
2020-07-21 12:40:45 +0600

    Generate build files

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import "./style.scss";
 
 import Edit from "./edit";
 import save from "./save";
+import icon from "./icon";
 
 registerBlockType("essential-blocks/countdown", {
 	title: __("Countdown", "essential-blocks"),
 	description: __("", "essential-blocks"),
 	category: "widgets",
-	icon: "smiley",
+	icon,
 	edit: Edit,
 	save,
 });
