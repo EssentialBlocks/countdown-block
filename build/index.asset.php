19f8c33593727b51a05d1cef9ca540e35ddb8078
Tasnim Alam <tasnimalamcse@gmail.com>
2021-02-08 14:54:18 +0600

    Generate build files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'd33d0fc6e9578d44690bf50d2506d0fb');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'f4b4c122936c90e7ff9a026931230eb6');
